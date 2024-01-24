58269ddd6d909ff7d9df3dd53c7785f53885f4c8
Peter <schmidt.multimedia@gmail.com>
2024-01-24 11:43:56 +0100

    Wertzuweisung / Felder

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,16 +4,34 @@ public class App {
     
     public static void main(String[] args) {
 
-        // Objekt entsteht ...
-        Cat cat = new Cat(); // Instanziierung
+        // Objekt erzeugen
+        Cat cat = new Cat(); 
         System.out.println(cat);
-        cat.tellYourAddress();
+
+        // Wertzuweisung
+        cat.name = "Alonzo";
+        cat.furColor = "grey";
+        cat.age = 35;
+
+        // Ausgabe
+        System.out.println(cat.name);
+        System.out.println(cat.furColor);
+        System.out.println(cat.age);
 
         System.out.println("---------------");
 
         Cat cat2 = new Cat();
         System.out.println(cat2);
-        cat2.tellYourAddress();
+      
+        // Wertzuweisung
+        cat2.name = "Grizabella";
+        cat2.furColor = "white";
+        cat2.age = 29;
+
+        // Ausgabe
+        System.out.println(cat2.name);
+        System.out.println(cat2.furColor);
+        System.out.println(cat2.age);
         
 
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,17 @@ package com.cc.java;
 
 public class Cat {
 
+    String name;
+    String furColor;
+    int age;
+
+
+
+    
+    public void tellYourName(){
+        System.out.println(this.name);
+    }
+    
     public void tellYourAddress(){
         System.out.println(this);
     }
