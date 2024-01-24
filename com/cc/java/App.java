cf39efa8858c45ead7c5c9ce685bba3698ce11bf
Peter <schmidt.multimedia@gmail.com>
2024-01-24 10:52:48 +0100

    Instanziierung mit Referenzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,18 @@ public class App {
     
     public static void main(String[] args) {
 
+        // Objekt entsteht ...
+        Cat cat = new Cat(); // Instanziierung
+        System.out.println(cat);
+        cat.tellYourAddress();
+
+        System.out.println("---------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
+        
+
 
     }
 
